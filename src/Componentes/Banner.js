be019dc8cc3a4ec8308f9d93348f290c2786103d
Nicolas <nicolas@MBP-de-Nicolas.lan>
2021-12-06 14:33:37 -0700

    Empezare a hacer la carta de presentacion

--- src/Componentes/Banner.js
+++ src/Componentes/Banner.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import banner from "../Multimedia/banner.jpg"
-
+import profile from "../Multimedia/profile.png"
 
 const Banner = () => {
     return (
         <div className="banner">
-            <img src={banner} alt="" />
-            
+            <img className="portada" src={banner} alt="" />
+            <img className="foto_perfil" src={profile} alt="" srcset="" />
         </div>
     )
 }
---
+++ src/Componentes/ProfileImage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import profile from "../Multimedia/profile.png"
+
+const ProfileImage = () => {
+    return (
+        <div className="profileImage">
+            <img src={profile} alt="" srcset="" />
+        </div>
+    )
+}
+
+export default ProfileImage
