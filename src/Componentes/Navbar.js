7c567dddd876655853ce9abcacd5f71e2eb9ee8d
Nicolas <nicolas@MBP-de-Nicolas.lan>
2021-12-06 14:16:39 -0700

    Se da inicio al portafolio

---
+++ src/Componentes/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <p>Nicolas's Cave</p>
+            <ul className>
+                <li>Home</li>
+                <li>About Me!</li>
+                <li>Projects</li>
+                <li>Skills</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
